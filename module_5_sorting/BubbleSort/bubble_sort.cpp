00e095b26d8f91b0db2be10a62ce1301963eb5ac
dgikonyo <dennis.gikonyo@jubileekenya.com>
2022-12-05 16:34:08 +0300

    bubble sort algo

---
+++ module_5_sorting/BubbleSort/bubble_sort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+/**
+ * Idea is to have the element with the maximum value at the (n-1)th position
+ */
+int bubble_sort(int x[], int n)
+{
+    // naive approach - two nested loops O(n**2) -> both best and worst cases!
+    /**
+     *
+     * loop I:
+     *  loop J = n - I - 1:
+     *      if (X[J] > X[J+1]):
+     *          temp = A[J]
+     *          x[J] = x[J+1]
+     *          x[J+1] = temp
+     */
+    bool elementSwapped;
+
+    for (int i = 0; i < n; i = i + 1)
+    {
+        elementSwapped = false;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (x[j] > x[j + 1])
+            {
+                int temp = A[j];
+                x[j] = x[j + 1];
+                x[j + 1] = true;
+                elementSwapped = true;
+            }
+        }
+
+        if (elementSwapped == false)
+            break;
+    }
+}
+
+/**
+ * traverse whole arrayto find smallest element
+*/
+int selection_sort(int x[])
+{
+}
+
+int main(void)
+{
+    return (0);
+}
